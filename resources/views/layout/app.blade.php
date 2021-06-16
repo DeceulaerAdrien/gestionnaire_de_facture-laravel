f99792af389b072db33f4716d05d9099f7ca6167
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-06-16 12:11:55 +0200

    css wip

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('test.css')}}">
+    <link rel="stylesheet" href="{{asset('styles/app.css')}}">
+    @yield('css')
     <title>@yield('title')</title>
 </head>
 <body>
