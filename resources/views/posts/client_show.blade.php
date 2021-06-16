b8e0405b01076df11745e8a5b5efcb8d683d6216
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-06-16 16:01:57 +0200

    .

--- resources/views/posts/client_show.blade.php
+++ resources/views/posts/client_show.blade.php
@@ -9,12 +9,19 @@
 @section('content')
 
 <div class="content">
-<h2>{{$client->société}}</h2>
-<p>Numéro de téléphone: {{$client->téléphone}}</p>
-<p>Email: {{$client->email}}</p>
-<p>Adresse: {{$client->adresse}}</p>
-<p>Numéro de TVA: {{$client->TVA}}</p>
-<p>Dernière facture: <a href="">{{$client->facture}}</a></p>
+    <div class="data">
+        <div class="text">
+            <h2>{{$client->société}}</h2>
+            <p>Numéro de téléphone: {{$client->téléphone}}</p>
+            <p>Email: {{$client->email}}</p>
+            <p>Adresse: {{$client->adresse}}</p>
+            <p>Numéro de TVA: {{$client->TVA}}</p>
+            <p>Dernière facture: <a href="">{{$client->facture}}</a></p>
+        </div>
+    </div>
+    <div class="deco">
+
+    </div>
 </div>
 
 @endsection
